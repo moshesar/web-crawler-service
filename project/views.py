bc5187a403ccd1bd1c5d4e3d808e4eb6c6ab8935
mosh <mozzmusic@gmail.com>
2023-05-14 21:32:53 +0300

    added re-crawl view in order to run a specific crawl again.

--- project/views.py
+++ project/views.py
@@ -25,6 +25,22 @@ def crawl() -> jsonify:
     return jsonify({'crawl_ids': ','.join(crawl_ids)}), 200
 
 
+@main.route('/recrawl/<int:crawl_id>', methods=['GET'])
+def re_crawl(crawl_id) -> jsonify:
+    """
+    Manually re-crawl an existing record.
+
+    :param crawl_id: The ID of the crawl.
+    :return: JSON response with crawl status or error message.
+    """
+    crawl_obj = get_crawl(crawl_id=crawl_id)
+    if not crawl_obj:
+        return jsonify({'error': 'Crawl ID not found.'}), 404
+    else:
+        process_crawl.delay(crawl_obj.id)
+        return jsonify({'crawl_ids': crawl_obj.id}), 200
+
+
 @main.route('/status/<int:crawl_id>', methods=['GET'])
 def get_status(crawl_id) -> jsonify:
     """
